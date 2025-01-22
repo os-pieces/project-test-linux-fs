9d178fd6a0d049ca38462ce921f5ee45b53a397c
heyuanjie87 <943313837@qq.com>
2025-01-22 21:42:23 +0800

    自动初始化

--- main.cpp
+++ main.cpp
@@ -2,15 +2,10 @@
 
 #include <QApplication>
 
-#include "test/test.h"
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-
-    test_fs_init();
-
     w.show();
     return a.exec();
 }
