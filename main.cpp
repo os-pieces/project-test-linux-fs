8534c4f246796cf18fb31578c1ef36674a578e6d
heyuanjie87 <943313837@qq.com>
2025-01-22 21:35:54 +0800

    初始化被测组件

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 
 #include <QApplication>
 
+#include "test/test.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    test_fs_init();
+
     w.show();
     return a.exec();
 }
